62edb21a55c65189ed413f1ab6f9b93d7af94c39
Bade Obari <42910678+o2ba@users.noreply.github.com>
2024-06-01 13:46:10 +0200

    Created classes to handle sensitive data, including hashing

---
+++ src/main/java/object/security/EncryptedData.java
@@ -0,0 +1,32 @@
+package object.security;
+
+/**
+ * Class for EncryptedObject. An EncryptedObject object is a representation of an object that has been encrypted.
+ * For example, a password or a revolving refresh token.
+ * The reason we have this class it to clearly distinguish between encrypted and non-encrypted objects.
+ * Does not have a toString override intentionally to prevent logging of encrypted data.
+ *
+ * @param encryptedData Encrypted Token
+ */
+public record EncryptedData(String encryptedData) {
+
+    /**
+     * Constructor for EncryptedObject.
+     *
+     * @param encryptedData Encrypted Token
+     */
+    public EncryptedData {
+        if (encryptedData == null) {
+            throw new IllegalArgumentException("Secret cannot be null");
+        }
+    }
+
+    /**
+     * Returns the encrypted data.
+     *
+     * @return the encrypted data
+     */
+    public String getEncryptedData() {
+        return encryptedData;
+    }
+}
---
+++ src/main/java/object/security/SensitiveData.java
@@ -0,0 +1,37 @@
+package object.security;
+
+import exception.register.ValidationException;
+
+public final class SensitiveData extends NonSensitiveData {
+
+    /**
+     * Constructor for SensitiveData.
+     *
+     * @param rawData the raw data
+     */
+    public SensitiveData(String rawData) {
+        super(rawData);
+    }
+
+    /**
+     * Validates the data. The password must be at least 8 characters long, contain at least one lowercase letter,
+     * one uppercase letter, one symbol and one digit.
+     * @throws ValidationException if the data is invalid
+     */
+    public void validateData() throws ValidationException {
+        if (!rawData.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,50}$")) {
+            throw new ValidationException("Password must be at least 8 characters long, " +
+                    "contain at least one lowercase letter, one uppercase letter, one symbol and one digit");
+        }
+    }
+
+    /**
+     * Disables the toString method for SensitiveData objects.
+     * @return Nothing. Throws an UnsupportedOperationException
+     * @throws UnsupportedOperationException if the method is called
+     */
+    @Override
+    public String toString() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Sensitive data cannot be converted to string");
+    }
+}
