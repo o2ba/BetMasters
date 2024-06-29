e39b6cd5d647b72499ae92566e3acdd77d4cc56c
Bade Obari <42910678+o2ba@users.noreply.github.com>
2024-06-29 11:39:36 +0200

    betting

---
+++ src/main/java/service/app/fixtureService/v2/common/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package service.app.fixtureService.v2.common.exception;
+
+public class NotFoundException
+extends Exception {
+        public NotFoundException(String message) {
+            super(message);
+        }
+}
