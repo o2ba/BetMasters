add282b7f064d5fff47ec18c728dbeb2957ae868
Bade Obari <42910678+o2ba@users.noreply.github.com>
2024-06-29 11:39:36 +0200

    betting

---
+++ src/main/java/service/app/authRequestService/simpleBettingService/exceptions/BadRequestException.java
@@ -0,0 +1,8 @@
+package service.app.authRequestService.simpleBettingService.exceptions;
+
+public class BadRequestException
+extends Exception {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
