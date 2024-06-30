08e6fab851b48087aa1d32dbfc859f1f887d17e5
Bade Obari <42910678+o2ba@users.noreply.github.com>
2024-06-30 18:14:30 +0200

    Fixed supported leagues

--- src/main/java/controller/fixture/SupportedLeagues.java
+++ src/main/java/controller/fixture/SupportedLeagues.java
@@ -16,6 +16,25 @@ public class SupportedLeagues {
 
     private ResourceLoader resourceLoader;
 
+    private String supportedLeagues = "[\n" +
+            "  {\n" +
+            "    \"name\": \"Serie A\",\n" +
+            "    \"id\": 71,\n" +
+            "    \"logo\": \"https://media.api-sports.io/football/leagues/71.png\",\n" +
+            "    \"country\": \"Brazil\",\n" +
+            "    \"country_flag\": \"https://media.api-sports.io/flags/br.svg\",\n" +
+            "    \"current_season\": \"2024\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\": \"Primeria Nacional\",\n" +
+            "    \"id\": 129,\n" +
+            "    \"logo\": \"https://media.api-sports.io/football/leagues/129.png\",\n" +
+            "    \"country\": \"Argentina\",\n" +
+            "    \"country_flag\": \"https://media.api-sports.io/flags/ar.svg\",\n" +
+            "    \"current_season\": \"2024\"\n" +
+            "  }\n" +
+            "]";
+
     @Autowired
     public SupportedLeagues(ResourceLoader resourceLoader) {
         this.resourceLoader = resourceLoader;
@@ -27,8 +46,12 @@ public class SupportedLeagues {
             Resource resource = resourceLoader.getResource("classpath:supportedLeagues.json");
             String content = Files.readString(Path.of(resource.getURI()));
             return ResponseEntity.ok(content);
-        } catch (IOException e) {
-            return ResponseEntity.status(500).body("Error reading file");
+        } catch (Exception e) {
+            try {
+                return ResponseEntity.ok(supportedLeagues);
+            } catch (Exception e2) {
+                return ResponseEntity.status(500).body(e2.getMessage());
+            }
         }
     }
 }
